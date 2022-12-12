b36721fb5106277a84cf64e135aba619402bf8ed
bennykurniawanlase <bennykurniawanlase@gmail.com>
2022-12-12 20:58:34 +0700

    Upload App ReactJS Search

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './app.css'
+import Table from './Table'
+import { Users } from './users'
+
+function App() {
+	const [query, setQuery] = useState('')
+
+	const keys = ['first_name', 'last_name', 'email']
+
+	console.log(Users[0]['email'])
+
+	const search = (data) => {
+		return data.filter((item) => keys.some((key) => item[key].toLowerCase().includes(query)))
+	}
+	return (
+		<div className="app">
+			<input
+				type="text"
+				className="search"
+				placeholder="Search here..."
+				onChange={(e) => setQuery(e.target.value)}
+			/>
+			<Table data={search(Users)} />
+		</div>
+	)
+}
+
+export default App
